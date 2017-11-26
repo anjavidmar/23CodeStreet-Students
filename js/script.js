064b4f6bf068e77fd18c589d5715222c12145a56
unknown <shotpurple@hotmail.com>
2017-11-26 00:46:29 +0000

    MS second upload

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ function helloWorld() {
 
     console.log('neonBeams');
 }
+
+ function reveal() {
+   var x = document.getElementById("mariannes");
+   if (x.style.display === "block") {
+       x.style.display = "none";
+   } else {
+      x.style.display = "block";
+   }
+ }
