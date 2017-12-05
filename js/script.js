1521a6c168457036daa54e9b003087ee4156a7d5
scottwooden <a.v.vidmar@gmail.com>
2017-12-05 17:44:44 +0000

    little tidy up

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
+// hannah's function
 function helloWorld() {
-    console.log('Laser blasts and force fields!');
-  }
-
-  function neonBeams() {
-
-    console.log('neonBeams');
+  console.log('Laser blasts and force fields!');
 }
 
- function reveal() {
-   var x = document.getElementById("mariannes");
-   if (x.style.display === "block") {
-       x.style.display = "none";
-   } else {
-      x.style.display = "block";
-   }
- }
+// shaheen's function
+function neonBeams() {
+  console.log('neonBeams');
+}
+
+// jenny's function
+
+// marianne's function
+function reveal() {
+  var x = document.getElementById("mariannes");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// anja's function
+function anjasPower() {
+  document.getElementById("anja").innerHTML = "In the dead of night, without making a sound, I make entire packets of cookies vanish into thin air.";
+}
